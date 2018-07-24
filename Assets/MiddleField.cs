78e228f3fda7ab68569c4dfbbe1508c09f53e126
Estafan Bakir <s3456790@student.rmit.edu.au>
2018-07-24 17:53:13 +1000

    First Full Attempt

---
+++ Assets/MiddleField.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using UnityEngine;
+using Meta;
+
+/// An example Monobehaviour which uses the gaze events to change the color
+/// of the game object it is assigned to.
+
+// Subclass of Meta objects
+public class MiddleField : MetaBehaviour, IGazeStartEvent, IGazeEndEvent
+{
+    /// Material for object which will be changed
+    private Material _renderMaterial;
+
+    /// Colour state when not viewed - i.e. dull
+    private Color _colorDull = new Color(0.05f, 0.05f, 0.05f, 0.0f);
+    
+    /// Colour state when is viewed - i.e. variable-colour
+    private Color _colorHighlighted;
+    
+    /// Interpolation variable which defines colour
+    private float _lambda = 0f;
+
+    /// Value of '_lambda' change with every 'update frame'
+    private float _delta = 0.05f;
+
+    /// Boolean for if object is being viewed.
+    private bool _bIsGazing;
+
+    /// Sets conditions when game starts
+    private void Start()
+    {
+        /// Reference to this object's material
+        Renderer renderer = GetComponent<Renderer>();
+        _renderMaterial = renderer.material;
+
+        /// Random colour for when viewed
+        _colorHighlighted = _renderMaterial.GetColor("_Color");
+    }
+
+    /// Sets conditions when game updates
+    private void Update()
+    {
+        ChangeColour();
+    }
+
+    /// Sets conditions when object is being viewed
+    public void OnGazeStart()
+    {
+        _bIsGazing = true;
+    }
+
+    /// Sets conditions when object is being viewed
+    public void OnGazeEnd()
+    {
+        _bIsGazing = false;
+    }
+
+    /// Changes colour every frame
+    private void ChangeColour()
+    {
+        /// Liniar Interpolation (LERP) bias, higher means higlights quicker
+        float lerpBias = 0.3f;
+
+        /// Vibrance increases if object is being gazed at
+        float sign = _bIsGazing ? 1 : -1;
+
+        /// Modifies lambda, incrementally changing the vibrance of the object. 
+        _lambda += (sign + lerpBias) * _delta;
+        
+        /// Allows lambda beyond 1 to glow longer. 
+        _lambda = Mathf.Clamp(_lambda, 0f, 2f); 
+
+        /// Calculate the color of the object. Lambda greater than 1 are clamped to 1.
+        Color color = Color.Lerp(
+            _colorDull, // colour A
+            _colorHighlighted, // colour B
+            Mathf.Clamp(_lambda, 0f, 1f) // float
+        );
+        _renderMaterial.color = color;
+        
+    }
+}
---
+++ Assets/Notes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta;
+
+
+public class Notes : MonoBehaviour {
+
+    /// Material for object which will be changed
+    private Material _renderMaterial;
+
+    /// Audio called when object triggered
+    public AudioClip AudioClip;
+    public AudioSource AudioSource;
+
+    /// Use this for initialization
+    void Start ()
+    {
+        /// Assign audio clip and volume
+        AudioSource.clip = AudioClip;
+        AudioSource.volume = 0.05f;
+        /// Reference to this object's material
+        Renderer renderer = GetComponent<Renderer>();
+        _renderMaterial = renderer.material;
+    }
+	
+	/// Update is called once per frame
+	void Update () {}
+
+    /// Actions taken when object is 'touched'
+    public void OneHandTouch ()
+    {
+        if (_renderMaterial.GetColor("_Color").Equals(Color.red))
+        {
+            _renderMaterial.SetColor("_Color", Color.green);
+        }
+        else
+        {
+            _renderMaterial.SetColor("_Color", Color.red);
+        }
+        AudioSource.Play();
+    }
+}
+
