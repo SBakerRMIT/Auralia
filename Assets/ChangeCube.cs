ff5e3cee3fe54257aa22b271c79520279210f9b0
Estafan Bakir <s3456790@student.rmit.edu.au>
2018-08-01 20:17:22 +1000

    Updates

---
+++ Assets/ChangeCube.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class ChangeCube : MonoBehaviour
+{
+    Renderer rend;
+    public Color altColor;
+
+    public Slider colorSliderRed;
+
+    float red;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    private void Start()
+    {
+        colorSliderRed.onValueChanged.AddListener( delegate
+        {
+            ValueChangeCheckRed();
+        });
+
+        altColor = rend.material.color;
+    }
+
+    public void ValueChangeCheckRed()
+    {
+        altColor.r = colorSliderRed.value;
+        rend.material.SetColor("_Color", altColor);
+    }
+
+
+
+    //public Slider redSlider;
+    //private Color ballColor;
+    //private Renderer myRenderer;
+    //private Material myMaterial;
+    //private bool pastStart = false;
+    //private int launchCount;
+
+
+    //public void Start()
+    //{
+    //    myRenderer = GetComponent<Renderer>();
+    //    myMaterial = myRenderer.material;
+    //    ballColor = myMaterial.color;
+    //    ballColor.r = 0;
+    //    ballColor.g = 0;
+    //    ballColor.b = 0;
+    //    redSlider.value = ballColor.r;
+    //    pastStart = true;
+    //}
+
+    //public void Update() { }
+
+    //// Handle slider changes
+    //public void UpdateSliders()
+    //{
+    //    if (!pastStart) { return; }
+
+    //    ballColor.r = redSlider.value;
+    //    myMaterial.color = ballColor;
+    //}
+}
